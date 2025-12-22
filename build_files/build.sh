4cd5aa9a02d08d5351eab5716868b68a7a68ef89
hikorin <hikorihawky@pm.me>
2025-12-22 22:23:16 +0800

    fix: build script 2 fix please i beg

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,3 +29,33 @@ systemctl enable podman.socket
 # Podman tools
 
 dnf5 install -y podman-compose podman-tui
+
+#### Numeric build scripts, based off of bazzite-dx
+
+CONTEXT_PATH="$(realpath "$(dirname "$0")/..")" # should return /run/context
+BUILD_SCRIPTS_PATH="$(realpath "$(dirname $0)")"
+MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
+SCRIPTS_PATH="$(realpath "$(dirname "$0")/scripts")"
+export CONTEXT_PATH
+export SCRIPTS_PATH
+export MAJOR_VERSION_NUMBER
+
+run_buildscripts_for() {
+	WHAT=$1
+	shift
+	# Complex "find" expression here since there might not be any overrides
+	# Allows us to numerically sort scripts by stuff like "01-packages.sh" or whatever
+	# CUSTOM_NAME is required if we dont need or want the automatic name
+	find "${BUILD_SCRIPTS_PATH}/$WHAT" -maxdepth 1 -iname "*-*.sh" -type f -print0 | sort --zero-terminated --sort=human-numeric | while IFS= read -r -d $'\0' script ; do
+		if [ "${CUSTOM_NAME}" != "" ] ; then
+			WHAT=$CUSTOM_NAME
+		fi
+		printf "::group:: ===$WHAT-%s===\n" "$(basename "$script")"
+		"$(realpath $script)"
+		printf "::endgroup::\n"
+	done
+}
+
+CUSTOM_NAME="base"
+run_buildscripts_for .
+CUSTOM_NAME="base"
