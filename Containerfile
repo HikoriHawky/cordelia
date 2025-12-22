33f3caa49e070068eaded41ae5ee6cc0d10662e6
hikorin <hikorihawky@pm.me>
2025-12-22 21:53:46 +0800

    feat: init image info

--- Containerfile
+++ Containerfile
@@ -5,6 +5,10 @@ COPY build_files /
 # Base Image
 FROM ghcr.io/ublue-os/bazzite:stable
 
+# Image Info
+ARG IMAGE_NAME="${IMAGE_NAME:-cordelia}"
+ARG IMAGE_VENDOR="{IMAGE_VENDOR:-hikorihawky}"
+
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
 # FROM ghcr.io/ublue-os/bluefin-nvidia:stable
---
+++ build_files/00-image-info.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eoux pipefail
+
+IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/$IMAGE_VENDOR/$IMAGE_NAME"
+
+IMAGE_PRETTY_NAME="Cordelia"
+
+# image-info File
+sed -i 's/"image-name": [^,]*/"image-name": "'"$IMAGE_NAME"'"/' $IMAGE_INFO
+sed -i 's|"image-vendor": [^,]*|"image-vendor": "'"$IMAGE_VENDOR"'"|' $IMAGE_INFO
+sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
+
+# OS Release File
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+sed -i "s/^PRETTY_NAME=.*/PRETTY_NAME=$IMGE_PRETTY_NAME" /usr/lib/os-release
+sed -i "s/^NAME=.*/NAME=$IMAGE_PRETTY_NAME/" /usr/lib/os-release
+sed -i "s/^DEFAULT_HOSTNAME=.*/DEFAULT_HOSTNAME=\"${IMAGE_PRETTY_NAME,}\"/" /usr/lib/os-release
+sed -i "s/^VARIANT_ID=.*/VARIANT_ID=$IMAGE_NAME/" /usr/lib/os-release
+
