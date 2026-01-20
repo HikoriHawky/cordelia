b0a41f067e5cd23e6f656dc7b0d81d83dd19d36c
HikoriHawky <hikorihawky@protonmail.com>
2026-01-20 15:12:27 +0800

    feat: bind-mount dirs for Nix/Guix

--- build_files/cordelia/17-cleanup.clj
+++ build_files/cordelia/17-cleanup.clj
@@ -1,6 +1,8 @@
 #!/usr/bin/env bb
 (require '[utils :refer [sh]])
 
+(sh "mkdir" "/nix" "/gnu")
+
 (sh "systemctl enable" "podman.socket")
 (sh "systemctl enable" "tailscaled.service")
 (sh "systemctl enable" "cordelia-groups.service")
