45c9c606c3bf7b3e6e82d087f690adb388f87bc9
HikoriHawky <hikorihawky@protonmail.com>
2026-01-26 15:56:55 +0800

    feat: foot term

--- build_files/04-packages.sh
+++ build_files/04-packages.sh
@@ -7,50 +7,50 @@ set -ouex pipefail
 #### Base packages from Fedora repos
 
 FEDORA_PACKAGES=(
-    ### From ublue-os/aurora
-    davfs2
-    ifuse
-    ksystemlog
-    #powertop
-    rclone
-    restic
-    setools
+	### From ublue-os/aurora
+	davfs2
+	ifuse
+	ksystemlog
+	#powertop
+	rclone
+	restic
+	setools
 
-    ### Emacs/NeoVim
-    emacs
-    libvterm
-    neovim
+	### Emacs/NeoVim
+	emacs
+	libvterm
+	neovim
 
-    ### CLI and tooling
-    ripgrep
-    fd-find
-    cmake
-    meson
-    libtool
-    vulkan-headers
-    vulkan-loader-devel
-    vulkan-utility-libraries-devel
+	### CLI and tooling
+	foot
+	ripgrep
+	fd-find
+	cmake
+	meson
+	libtool
+	vulkan-headers
+	vulkan-loader-devel
+	vulkan-utility-libraries-devel
 
+	### BorgBackup
+	borgbackup
+	borgmatic
 
-    ### BorgBackup
-    borgbackup
-    borgmatic
+	### Podman and Containers
+	podman-compose
+	podman-tui
 
-    ### Podman and Containers
-    podman-compose
-    podman-tui
+	### SDL
+	SDL2
+	SDL2_image
+	SDL2_ttf
+	SDL2_mixer
 
-    ### SDL
-    SDL2
-    SDL2_image
-    SDL2_ttf
-    SDL2_mixer
-
-    ### AMD ROCm
-    rocminfo
-    rocm-opencl
-    rocm-hip
-    rocm-runtime
+	### AMD ROCm
+	rocminfo
+	rocm-opencl
+	rocm-hip
+	rocm-runtime
 )
 
 # Install all Fedora packages
@@ -69,15 +69,15 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc
 EOF
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/vscode.repo
 dnf -y install --enablerepo=code \
-    code
+	code
 
 # Install additional KDE themeing packages
 echo "Installing KDE theming packages from openSUSE Build Service repos..."
 wget -O /etc/yum.repos.d/home:luisbocanegra.repo https://download.opensuse.org/repositories/home:luisbocanegra/Fedora_43/home:luisbocanegra.repo
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/home:luisbocanegra.repo
 dnf -y install --enablerepo=home_luisbocanegra \
-    kde-material-you-colors
+	kde-material-you-colors
 wget -O /etc/yum.repos.d/home:paul4us.repo https://download.opensuse.org/repositories/home:paul4us/Fedora_43/home:paul4us.repo
 sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/home:paul4us.repo
 dnf -y install --enablerepo=home_paul4us \
-    klassy
+	klassy
